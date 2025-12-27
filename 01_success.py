20aead30ba2d67a21a945656c0a976cb9ebb918f
Patrick Desjardins <mrdesjardins@gmail.com>
2025-12-27 15:25:10 -0800

    Add time start and stop for 01_success

--- 01_success.py
+++ 01_success.py
@@ -1,23 +1,26 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 from common import run_simulation_mp
 
-# simulation_data = run_simulation()
+start_time = time.perf_counter()
 simulation_data = run_simulation_mp(
     n_years=40,
     return_trajectories=False,
     n_sims=10_000_000,
     initial_balance=5_000_000,
-    withdrawal=100_000,
-    withdrawal_negative_year=90_000,
-    random_with_real_life_constraints=True,
+    withdrawal=90_000,
+    withdrawal_negative_year=80_000,
+    random_with_real_life_constraints=False,
     sp500_percentage=0.65,
     bond_rate=0.03,
     inflation_rate=0.03,
     social_security_money=45_000,
     years_without_social_security=20,
 )
+end_time = time.perf_counter()
+print(f"Total optimization time: {end_time - start_time:.2f} seconds")
 simulation_data.print_stats()
 final_balances = simulation_data.final_balances
 n_sims = simulation_data.n_sims
